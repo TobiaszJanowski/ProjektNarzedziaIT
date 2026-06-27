360d0d3d04779a267fdc97e979115568a01cb43e
Tobiasz <dsw60319@student.dsw.edu.pl>
2026-06-27 19:43:53 +0200

    Task1

---
+++ ui.py
@@ -0,0 +1,35 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel
+from PyQt6.QtCore import QThread, pyqtSignal
+from converter import load_data, save_data
+
+class ConvertWorker(QThread):
+    finished = pyqtSignal(str)
+    def __init__(self, src, dst):
+        super().__init__()
+        self.src, self.dst = src, dst
+    def run(self):
+        try:
+            data = load_data(self.src)
+            save_data(data, self.dst)
+            self.finished.emit("Sukces!")
+        except Exception as e:
+            self.finished.emit(f"Błąd: {str(e)}")
+
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        self.btn = QPushButton("Konwertuj (Asynchronicznie)")
+        self.btn.clicked.connect(self.start_conversion)
+        layout.addWidget(self.btn)
+        self.setLayout(layout)
+    def start_conversion(self):
+        self.worker = ConvertWorker("in.json", "out.yml")
+        self.worker.finished.connect(lambda msg: print(msg))
+        self.worker.start()
+
+app = QApplication(sys.argv)
+win = App()
+win.show()
+sys.exit(app.exec())
